2264ca347ded3102c5ae787594c3ec788f5f5da5
Lars <Lars@DESKTOP-1788R46>
2020-12-25 21:30:34 +0100

    Siteswap Validierung

--- csharptestfunction/FunctionHandler.cs
+++ csharptestfunction/FunctionHandler.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Http;
 using System.IO;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using System;
 
 namespace Function
 {
@@ -10,8 +13,57 @@ namespace Function
         {
             var reader = new StreamReader(request.Body);
             var input = await reader.ReadToEndAsync();
-
-            return (200, $"Hello! Your input was {input}");
+            var isValidSiteswap = Siteswap.TryCreate(input, out var siteswap);
+            return (200, $"Hello! Your input {input}. Dies ist {(isValidSiteswap? "ein":"kein")} valider Siteswap");
         }
+    }
+
+    public class Siteswap
+    {
+        public int[] Array { get; }
+        public Siteswap(int[] array) 
+        {
+            this.Array = array;
+        }
+        public static bool TryCreate(string input, out Siteswap siteswap) 
+        {
+			if (TryCreate(input,out int[] array))
+			{
+                siteswap = new Siteswap(array);
+                return array.Select((i,x)=> x+i).Distinct().Count() == array.Count();
+			}
+            siteswap = null;
+            return false;
+        }
+
+        private static bool TryCreate(string input, out int[] array) 
+        {
+            try
+            {
+
+                array = input.Select(x => CharToSiteswapValue(x)).ToArray();
+                return true;
+            }
+            catch (Exception e) 
+            {
+                array = new int[]{};
+                return false;
+            }
+        }
+
+        private static int CharToSiteswapValue(char c) 
+        {
+            var isInt = int.TryParse(c.ToString(), out var i);
+			if (isInt)
+			{
+                return i;
+			}
+            return Convert.ToInt32(Encoding.ASCII.GetBytes(c.ToString()).Single());
+        }
+
+		public Siteswap()
+		{
+
+		}
     }
 }
