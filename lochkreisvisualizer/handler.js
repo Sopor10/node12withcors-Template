9ea0dcea05b809dc2d932520c8e0888ade594a0f
Lars <Lars@DESKTOP-1788R46>
2021-01-16 18:19:44 +0100

    base64 encoding

--- lochkreisvisualizer/handler.js
+++ lochkreisvisualizer/handler.js
@@ -1,8 +1,8 @@
 'use strict'
 
 module.exports = async (event, context) => {
-  const result = '<svg viewBox="-105 -105 210 210" width="520" height="520"><circle cx="0" cy="0" r="100" fill="none" stroke="black" stroke-width="1"></circle><circle cx="0" cy="0" r="25" fill="none" stroke="orange" stroke-width="1"></circle><circle cx="0" cy="0" r="75" fill="none" stroke="green" stroke-width="1" stroke-dasharray="5,5"></circle><circle cx="75" cy="-1.8369701987210297e-14" r="10" fill="none" stroke="blue" stroke-width="1"></circle></svg>';
-
+  const svg = atob('<svg viewBox="-105 -105 210 210" width="520" height="520"><circle cx="0" cy="0" r="100" fill="none" stroke="black" stroke-width="1"></circle><circle cx="0" cy="0" r="25" fill="none" stroke="orange" stroke-width="1"></circle><circle cx="0" cy="0" r="75" fill="none" stroke="green" stroke-width="1" stroke-dasharray="5,5"></circle><circle cx="75" cy="-1.8369701987210297e-14" r="10" fill="none" stroke="blue" stroke-width="1"></circle></svg>');
+  const result = "data:image/svg+xml;base64," + svg;
   return context
     .status(200)
     .succeed(result)
