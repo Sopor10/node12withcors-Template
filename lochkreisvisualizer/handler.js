61b61908e2c32c1a7971352cc3cb4f1187153cd5
Lars <Lars@DESKTOP-1788R46>
2021-01-16 19:43:52 +0100

    changed size

--- lochkreisvisualizer/handler.js
+++ lochkreisvisualizer/handler.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = async (event, context) => {
-  const svg = '<svg viewBox="-105 -105 210 210" width="520" height="520"><circle cx="0" cy="0" r="100" fill="none" stroke="black" stroke-width="1"></circle><circle cx="0" cy="0" r="25" fill="none" stroke="orange" stroke-width="1"></circle><circle cx="0" cy="0" r="75" fill="none" stroke="green" stroke-width="1" stroke-dasharray="5,5"></circle><circle cx="75" cy="-1.8369701987210297e-14" r="10" fill="none" stroke="blue" stroke-width="1"></circle></svg>';
+  const svg = '<svg viewBox="-105 -105 210 210" width="400" height="400"><circle cx="0" cy="0" r="100" fill="none" stroke="black" stroke-width="1"></circle><circle cx="0" cy="0" r="25" fill="none" stroke="orange" stroke-width="1"></circle><circle cx="0" cy="0" r="75" fill="none" stroke="green" stroke-width="1" stroke-dasharray="5,5"></circle><circle cx="75" cy="-1.8369701987210297e-14" r="10" fill="none" stroke="blue" stroke-width="1"></circle></svg>';
   let buff = new Buffer(svg);
   let base64data = buff.toString('base64');
   const result = "data:image/svg+xml;charset=UTF-8," + svg;
